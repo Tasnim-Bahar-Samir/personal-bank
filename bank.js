f35c6ef7b2bf7cd8af8384119aa1ee09da4ee6ae
Tasnim-Bahar-Samir <tanimsamir3@gmail.com>
2022-08-16 02:16:00 +0600

    login section added

---
+++ bank.js
@@ -0,0 +1,9 @@
+document.getElementById('submit-btn').addEventListener('click',function(){
+    const emailField = document.getElementById('email-field');
+    const passwordField = document.getElementById('password-field');
+    if(emailField.value === 'personalBank@samir.com' && passwordField.value === '123456'){
+        window.location.href = 'transaction.html'
+    }else{
+        alert('invalid input!')
+    }
+})
